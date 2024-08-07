3432c563f2ade02439fced85790debf978627a11
Kabin-stha <www.kabinkabin321@gmail.com>
2024-08-07 11:18:29 +0545

    august

---
+++ day_1.js
@@ -0,0 +1,20 @@
+
+// function first(){
+//     let second=2;
+//     let third =2;
+//     let result=second+third
+//    return result
+
+// }
+
+
+// console.log(first());
+
+// function second(a,b){
+//     return compare =a-b
+// }
+// console.log(second(4,1))
+
+function handleClickExternal(){
+    alert("external js")
+}
